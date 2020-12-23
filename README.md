adfc3a26d4c8db4f570b263708fb52b56e10da80
Tejesh Mehta <tejesh.mehta@gmail.com>
2020-12-23 00:06:35 -0800

    initial implementation with docs and tests

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Name
+# abortable-generator
 
-Description
+Abortable AsyncGenerators using AbortControllers
 
 # Installation
 
@@ -14,9 +14,190 @@ npm i --save node-module-template
 
 ```js
 // esm
-import nmt from 'node-module-template`
+import abortable from 'abortable-generator`
 // commonjs
-const nmt = require('node-module-template')
+const abortable = require('abortable-generator').default
+```
+
+#### Problem with "stuck" AsyncGenerators
+
+There is a problem with AsyncGenerators where unsettled Promises can prevent them from finishing (and sometimes lead to memory leaks 😱)
+
+```js
+async function* getItems() {
+  try {
+    yield new Promise((resolve) => {})
+  } finally {
+    // this never happens
+  }
+}
+
+const items = getItems()
+
+setTimeout(() => {
+  // some time later
+  items.return()
+}, 100)
+for await (let item of items) {
+  // this never happens
+}
+```
+
+#### Abort "stuck" AsyncGenerators w/ return or throw
+
+Here's an example of how to abort "stuck" AsyncGenerators using abortable-generator
+
+```js
+import abortable from 'abortable-generators'
+
+// wrap your AsyncGenerators with `abortable` and use `raceAbort` inside
+const getItems = abortable(async function* (raceAbort) {
+  try {
+    yield raceAbort(new Promise((resolve) => {}))
+  } catch (err) {
+    if (err.name === 'AbortError') {
+      // note: abort-errors should be handled and ignored!
+      return
+    }
+    throw err
+  } finally {
+    // return or throw will reach here!
+  }
+})
+const items = getItems()
+
+setTimeout(() => {
+  // some time later
+  // return will trigger an abort
+  items.return()
+  // throw will too and throw an error inside the generator
+  items.throw(new Error('boom'))
+}, 100)
+for await (let item of items) {
+  // generator didn't yield anything
+}
+```
+
+#### Abort "stuck" AsyncGenerators w/ an AbortSignal
+
+Here's an example of how to abort "stuck" AsyncGenerators using abortable-generator
+
+```js
+import abortable from 'abortable-generators'
+
+const controller = new AbortController()
+const signal = controller.signal
+
+// wrap your AsyncGenerators with `abortable` and use `raceAbort` inside
+const getItems = abortable(async function* (raceAbort) {
+  try {
+    yield raceAbort(new Promise((resolve) => {}))
+  } catch (err) {
+    if (err.name === 'AbortError') {
+      // note: abort-errors should be handled and ignored!
+      return
+    }
+    throw err
+  } finally {
+    // will reach here!
+  }
+})
+
+// this signal can also be used to abort the generator!
+const items = getItems(signal)
+
+setTimeout(() => {
+  // some time later
+  controller.abort()
+}, 100)
+for await (let item of items) {
+  // generator didn't yield anything
+}
+```
+
+#### Abort AsyncGenerators containing cancellable Promises
+
+Here's an example of how to abort AsyncGenerators that contain cancellable Promises (supporting AbortSignal)
+
+```js
+import abortable from 'abortable-generators'
+
+const getItems = abortable(async function* (raceAbort) {
+  try {
+    yield raceAbort((signal) => fetch('https://codeshare.io', { signal }))
+  } catch (err) {
+    if (err.name === 'AbortError') {
+      // note: abort-errors should be handled and ignored!
+      return
+    }
+    throw err
+  } finally {
+    // return or throw will reach here!
+  }
+})
+const items = getItems()
+
+setTimeout(() => {
+  // some time later
+  items.return()
+}, 100)
+for await (let item of items) {
+  // generator didn't yield anything
+}
+```
+
+#### Advanced Example: Abort nested AsyncGenerators w/ multiple Promises and external AbortSignal
+
+Here's an example of how to abort AsyncGenerators that contain cancellable Promises (supporting AbortSignal)
+
+```js
+import abortable from 'abortable-generators'
+
+const controller = new AbortController()
+const signal = controller.signal
+
+const getMoreItems = abortable(async function* (raceAbort) {
+  try {
+    await raceAbort(timeoutPromise(100))
+    yield raceAbort((signal) => fetch('https://codeshare.io', { signal }))
+  } catch (err) {
+    if (err.name === 'AbortError') {
+      // note: abort-errors should be handled and ignored!
+      return
+    }
+    throw err
+  } finally {
+    // return or throw will reach here!
+  }
+})
+const getItems = abortable(async function* (raceAbort) {
+  try {
+    const moreItems = getMoreItems(raceAbort)
+    for await (let item in moreItems) {
+      yield item
+    }
+    await raceAbort(timeoutPromise(100))
+    yield raceAbort((signal) => fetch('https://codeshare.io', { signal }))
+  } catch (err) {
+    if (err.name === 'AbortError') {
+      // note: abort-errors should be handled and ignored!
+      return
+    }
+    throw err
+  } finally {
+    // return or throw will reach here!
+  }
+})
+// this signal can also be used to abort the generator!
+const items = getItems(signal)
+
+setTimeout(() => {
+  // some time later
+  items.return()
+}, 100)
+for await (let item of items) {
+  //
+}
 ```
 
 # License
--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,1 +1,339 @@
+import abortable, { setAbortController } from '../index'
 
+import AbortController from 'abort-controller'
+import BaseError from 'baseerr'
+
+setAbortController(AbortController)
+
+describe('abortable', () => {
+  it('should wrap an async generator', async () => {
+    const gen = abortable(async function* (raceAbort) {
+      yield 1
+      yield 2
+      yield 3
+    })
+    const items = []
+
+    for await (let item of gen()) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+        3,
+      ]
+    `)
+  })
+
+  it('should wrap an async generator and abort promises when returned', async () => {
+    const createGen = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(1)
+        yield Promise.resolve(2)
+        yield raceAbort(new Promise((resolve) => {}))
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        throw err
+      }
+    })
+    const items = []
+    const gen = createGen()
+
+    setTimeout(() => {
+      gen.return()
+    }, 10)
+    for await (let item of gen) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+      ]
+    `)
+  })
+
+  it('should wrap an async generator and abort cancellable promises (via signal) when returned', async () => {
+    const createGen = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(1)
+        yield Promise.resolve(2)
+        yield raceAbort((signal) => mockFetch({ signal }))
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        throw err
+      }
+    })
+    const items = []
+    const gen = createGen()
+
+    setTimeout(() => {
+      gen.return()
+    }, 10)
+    for await (let item of gen) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+      ]
+    `)
+  })
+
+  it('should wrap an async generator and abort cancellable promises (via signal) when thrown', async () => {
+    const createGen = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(1)
+        yield Promise.resolve(2)
+        yield raceAbort((signal) => mockFetch({ signal }))
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        throw err
+      }
+    })
+    const items = []
+    const gen = createGen()
+
+    setTimeout(() => {
+      gen.return()
+    }, 10)
+    for await (let item of gen) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+      ]
+    `)
+  })
+
+  it('should wrap an async generator and abort promises when thrown', async () => {
+    const createGen = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(1)
+        yield Promise.resolve(2)
+        yield raceAbort(new Promise((resolve) => {}))
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        items.push(err)
+      }
+    })
+    const items = []
+    const gen = createGen()
+
+    setTimeout(() => {
+      gen.throw(new Error('boom')).catch((err) => {})
+    }, 10)
+    for await (let item of gen) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+        [Error: boom],
+      ]
+    `)
+  })
+
+  it('should wrap an async generator and abort promises via signal', async () => {
+    const createGen = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(1)
+        yield Promise.resolve(2)
+        yield raceAbort(new Promise((resolve) => {}))
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        throw err
+      }
+    })
+    const items = []
+    const controller = new AbortController()
+    const signal = controller.signal
+    const gen = createGen(signal)
+
+    setTimeout(() => {
+      controller.abort()
+    }, 10)
+    for await (let item of gen) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+      ]
+    `)
+  })
+
+  it('should wrap an async generator and abort promises via aborted signal', async () => {
+    const createGen = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(1)
+        yield Promise.resolve(2)
+        yield raceAbort(new Promise((resolve) => {}))
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        throw err
+      }
+    })
+    const items = []
+    const controller = new AbortController()
+    const signal = controller.signal
+    const gen = createGen(signal)
+
+    controller.abort()
+    for await (let item of gen) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`Array []`)
+  })
+
+  it('should make nesting async generators easy', async () => {
+    const items = []
+    const createGen2 = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(4)
+        yield Promise.resolve(5)
+        yield raceAbort(new Promise((resolve) => {}))
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        throw err
+      } finally {
+        items.push('finally2')
+      }
+    })
+    const createGen1 = abortable(async function* (raceAbort) {
+      try {
+        yield Promise.resolve(1)
+        yield Promise.resolve(2)
+        yield Promise.resolve(3)
+        const gen2 = createGen2(raceAbort)
+        for await (let result of gen2) {
+          yield result
+        }
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        items.push(err)
+      } finally {
+        items.push('finally1')
+      }
+    })
+
+    const gen = createGen1()
+
+    setTimeout(() => {
+      gen.throw(new Error('boom')).catch((err) => {})
+    }, 10)
+    for await (let item of gen) {
+      items.push(item)
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+        3,
+        4,
+        5,
+        "finally2",
+        [Error: boom],
+        "finally1",
+      ]
+    `)
+  })
+
+  it('baseline generator and test for return', async () => {
+    const createGen = function* () {
+      try {
+        yield 1
+        yield 2
+        yield 3
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        items.push(err)
+      }
+    }
+    const items = []
+    const gen = createGen()
+
+    for await (let item of gen) {
+      items.push(item)
+      if (items.length === 2) {
+        try {
+          gen.return()
+        } catch (err) {}
+      }
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+      ]
+    `)
+  })
+
+  it('baseline generator and test for throw', async () => {
+    const createGen = function* () {
+      try {
+        yield 1
+        yield 2
+        yield 3
+      } catch (err) {
+        if (err.name === 'AbortError') return
+        items.push(err)
+      }
+    }
+    const items = []
+    const gen = createGen()
+
+    for await (let item of gen) {
+      items.push(item)
+      if (items.length === 2) {
+        try {
+          gen.throw(new Error('boom'))
+        } catch (err) {
+          console.log('ERR', err)
+        }
+      }
+    }
+
+    expect(items).toMatchInlineSnapshot(`
+      Array [
+        1,
+        2,
+        [Error: boom],
+      ]
+    `)
+  })
+})
+
+// function identity(v) {
+//   return v
+// }
+// function noop(v) {
+//   return function () {
+//     return v({ raceAbort: identity })
+//   }
+// }
+class AbortError extends BaseError<{}> {}
+function mockFetch({ signal }: { signal: AbortSignal }) {
+  return new Promise((resolve, reject) => {
+    function abort() {
+      reject(new AbortError('aborted'))
+    }
+    if (signal.aborted) return void abort()
+    signal.addEventListener('abort', abort)
+  })
+}
